79a89acdc713eceb24a8979ae85ce28152108e4c
Linnea Svensson <svensson.47@hotmail.com>
2017-11-06 16:05:37 +0100

    value, gameOver, winner

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -16,7 +16,9 @@ empty = Empty
 -- | A ranks value according to Black Jack rules
 valueRank :: Rank -> Integer
 valueRank Ace = 11
-valueRank (Numeric x) = x
+valueRank (Numeric x) |x < 2 = 2
+                      |x > 10 = 10
+                      |otherwise = x
 valueRank _ = 10
 
 -- | A cards value according to Black Jack rules
@@ -29,10 +31,26 @@ numberOfAces Empty = 0
 numberOfAces (Add (Card Ace s) hand) = 1 + numberOfAces hand
 numberOfAces (Add card hand) = 0 + numberOfAces hand
 
---value :: Hand -> Integer
---value Empty = 0
---value (Add card hand) = __ + value hand
+-- | The total value of a player's hand
+-- | ace counts as 11
+value' :: Hand -> Integer
+value' Empty = 0
+value' (Add card hand) = valueCard card + value hand
 
---gameOver :: Hand -> Bool
+-- | Choose the highest value that is still < 21
+value :: Hand -> Integer
+value hand  |v > 21 = v - n*10
+            |otherwise = v
+          where
+            n = numberOfAces hand
+            v = value' hand
 
---winner :: Hand -> Hand -> Player
+-- | The game is over if the hand's value is > 21
+gameOver :: Hand -> Bool
+gameOver hand = value hand > 21
+
+winner :: Hand -> Hand -> Player
+winner guest bank | gameOver guest = Bank
+                  | gameOver bank = Guest
+                  | value guest <= value bank = Bank
+                  | otherwise = Guest

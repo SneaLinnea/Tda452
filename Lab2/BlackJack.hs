7c76736db97fa72565321cba8f82a984d5e1f8da
Linnea Svensson <svensson.47@hotmail.com>
2017-11-14 10:39:04 +0100

    sameSuit help function

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -1,6 +1,8 @@
 module BlackJack where
 import Cards
 import RunGame
+
+import Test.QuickCheck
 {-Task 3.2
 size hand2
   = size (Add(Card (Numeric 2) Hearts)
@@ -13,6 +15,10 @@ size hand2
 empty :: Hand
 empty = Empty
 
+-- | Property: an empty hand is empty
+prop_empty :: Bool
+prop_empty = Empty == empty
+
 -- | A ranks value according to Black Jack rules
 valueRank :: Rank -> Integer
 valueRank Ace = 11
@@ -20,6 +26,11 @@ valueRank (Numeric x) |x < 2 = 2
                       |x > 10 = 10
                       |otherwise = x
 valueRank _ = 10
+
+-- | Property: value of Rank is >= 2 and <= 11
+prop_valueTwoEleven :: Rank -> Bool
+prop_valueTwoEleven r = (v <= 11) && (v >= 2)
+  where v = valueRank r
 
 -- | A cards value according to Black Jack rules
 valueCard :: Card -> Integer
@@ -30,6 +41,10 @@ numberOfAces :: Hand -> Integer
 numberOfAces Empty = 0
 numberOfAces (Add (Card Ace s) hand) = 1 + numberOfAces hand
 numberOfAces (Add card hand) = 0 + numberOfAces hand
+
+-- | Property: number of aces <= than number of cards
+prop_acesFewer :: Hand -> Bool
+prop_acesFewer h = (numberOfAces h <= size h) && (numberOfAces h >= 0)
 
 -- | The total value of a player's hand
 -- | ace counts as 11
@@ -46,6 +61,13 @@ value hand  |v > 21 = v - n*10
             n = numberOfAces hand
             v = value' hand
 
+-- | Property: the value of a hand is less than the value with maxed Aces
+prop_valueAndAces :: Hand -> Bool
+prop_valueAndAces h | n == 0    = value h == value' h
+                    | n == 1    = value h <= value' h
+                    | otherwise = value h <  value' h
+                where n = numberOfAces h
+
 -- | The game is over if the hand's value is > 21
 gameOver :: Hand -> Bool
 gameOver hand = value hand > 21
@@ -57,3 +79,49 @@ winner guest bank | gameOver guest = Bank
                   | gameOver bank = Guest
                   | value guest <= value bank = Bank
                   | otherwise = Guest
+
+-- | Property: Given equally valued hands the bank allways wins
+prop_bankWinEqual :: Hand -> Hand -> Property
+prop_bankWinEqual guest bank = value guest == value bank ==>
+                                       winner guest bank == Bank
+
+-- | Property: If the player busts the bank wins
+prop_bankWinBust :: Hand -> Hand -> Property
+prop_bankWinBust guest bank = gameOver guest ==> winner guest bank == Bank
+
+-- | Given two hands, <+ puts the first one on top of the second one
+(<+) :: Hand -> Hand -> Hand
+hand1 <+ Empty = hand1
+Empty <+ hand2 = hand2
+(Add card hand) <+ hand2 = Add card (hand <+ hand2)
+
+
+-- | Property: <+ should be associative
+prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool
+prop_onTopOf_assoc p1 p2 p3 =
+    p1<+(p2<+p3) == (p1<+p2)<+p3
+
+-- | Property: size of the combined hand should be the sum of the sizes
+-- | of the two individual hands
+prop_size_onTopOf :: Hand -> Hand -> Bool
+prop_size_onTopOf p1 p2 =
+    (size p1 + size p2) == size (p1 <+ p2)
+
+--suitNumbers xs = foldr (<+)
+
+-- | Given a suit, return all cards in that suit
+sameSuit :: Suit -> Hand
+sameSuit s = foldr Add Empty cards
+    where
+      cards = [Card r s | r <- [Numeric n | n <- [2..10]] ++
+              [Jack, Queen, King, Ace]]
+
+  --(Add(Card (Numeric n ) s)
+
+--(Add (Card Jack s) (Add (Card Queen s)
+  --           (Add (Card King s) (Add (Card Ace s) Empty)))))
+    --            where
+      --            n = [2..10]
+
+-- | A function that returns a full deck of cards
+--fullDeck :: Hand
